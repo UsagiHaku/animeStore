f0194d50657ea72ad426adfa82088a2030efe1e2
Migdonio Rodriguez <raulmigdonio@gmail.com>
2019-12-03 19:50:31 -0600

    Update login and signup screen

--- app/src/main/java/com/example/mocks/SignUpActivity.kt
+++ app/src/main/java/com/example/mocks/SignUpActivity.kt
@@ -3,11 +3,16 @@ package com.example.mocks
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.animestore.R
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        backButton?.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
